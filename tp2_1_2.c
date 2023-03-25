c7592091fcd2a3b514135b0d7c10767be20beba6
0ignacio <ignaciogonzaley@gmail.com>
2023-03-25 16:56:30 -0300

    correcion punto 2.d

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -2,15 +2,20 @@
 #include <time.h>
 
 #define N 20
+#define M 20
 
 int main(){
-    int i;
-    double vt[N];
-    double *pvt = vt;
-    for(i = 0; i<N; i++){
-        *pvt = 1 + rand() %100;
-        printf("%.2f\n", *pvt);
-        pvt++;
+    int i, j;
+    double vt[N][M];
+    double *pvt = &vt[0][0];
+    srand(time(NULL));
+    for(i = 0; i < N; i++){
+        for(i = 0; i < M; j++){
+            *(pvt + i * M + j) = 1 + rand() %100;
+            printf("%.2f\t", *(pvt + i *M + j));
+        }
+        printf("\n");
     }
     return 0;
 }
+
